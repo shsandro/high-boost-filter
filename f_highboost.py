82dd53307febe3a362d734ad1f5458c729370ec2
shsandro <shsandro@github.com>
2020-12-01 23:41:57 -0300

    modificações

--- f_highboost.py
+++ f_highboost.py
@@ -22,8 +22,8 @@ freq_kernel = fp.fft2(fp.ifftshift(gauss_kernel))
 assert(freq_img.shape == gauss_kernel.shape)
 
 # aplica convolução (borramento)
-convolved = freq_img * freq_kernel
-blured_img = fp.ifft2(convolved).real
+convolved_img = freq_img * freq_kernel
+blured_img = fp.ifft2(convolved_img).real
 
 blured_img = np.abs(blured_img)
 blured_img = blured_img * 255 / blured_img.max()
@@ -44,13 +44,12 @@ pylab.subplot(2, 2, 2), pylab.imshow(
 pylab.title('Transformada do filtro', size=10), pylab.axis('off')
 
 pylab.subplot(2, 2, 3), pylab.imshow(
-    (20*np.log10(0.1 + fp.fftshift(convolved))).astype(int))
+    (20*np.log10(0.1 + fp.fftshift(convolved_img))).astype(int))
 pylab.title('Imagem borrada no domínio da frequência',
             size=10), pylab.axis('off')
 
 pylab.subplots_adjust(wspace=0.2, hspace=1)
 pylab.show()
-
 ##################################################################################################
 
 # lê o valor de k
@@ -59,6 +58,20 @@ k = int(input("Digite o valor de k: "))
 # obtem a máscara (arestas)
 mask = cv2.subtract(img, blured_img)
 mask = k * mask
+
+####################### Mostra todos os passos da convolução #######################
+pylab.subplot(2, 3, 1), pylab.imshow(img), pylab.title(
+    'Imagem original', size=10), pylab.axis('off')
+
+pylab.subplot(2, 3, 3), pylab.imshow(blured_img)
+pylab.title('Imagem após convolução', size=10), pylab.axis('off')
+
+pylab.subplot(2, 3, 5), pylab.imshow(mask)
+pylab.title('Arestas para aguçamento', size=10), pylab.axis('off')
+
+pylab.subplots_adjust(wspace=0.2, hspace=1)
+pylab.show()
+##################################################################################################
 
 # obtem imagem final
 final_image = cv2.add(img, mask)
