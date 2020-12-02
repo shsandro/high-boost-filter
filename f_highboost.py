b82b77b8167ff892025afbde6a1dcd4456cf6e36
shsandro <shsandro@github.com>
2020-12-01 23:13:35 -0300

    highboost na frequancia implementado

---
+++ f_highboost.py
@@ -0,0 +1,66 @@
+import scipy.fftpack as fp
+from scipy import signal
+import numpy as np
+import numpy.fft
+import cv2
+import matplotlib.pylab as pylab
+
+
+# carrega a imagem
+img = cv2.imread(input("Digite o caminho para a imagem de entrada: "))[:, :, 0]
+
+# lê o valor de D
+D = int(input("Digite o valor de D para o kernel: "))
+
+# obtém o filtro gaussiano no domínio espacial
+gauss_kernel = np.outer(signal.gaussian(
+    img.shape[0], D), signal.gaussian(img.shape[1], D))
+
+# obtém a imagem e o filtro no domínio da frquência
+freq_img = fp.fft2(img)
+freq_kernel = fp.fft2(fp.ifftshift(gauss_kernel))
+assert(freq_img.shape == gauss_kernel.shape)
+
+# aplica convolução (borramento)
+convolved = freq_img * freq_kernel
+blured_img = fp.ifft2(convolved).real
+
+blured_img = np.abs(blured_img)
+blured_img = blured_img * 255 / blured_img.max()
+blured_img = blured_img.astype(np.uint8)
+
+####################### Mostra todos os passos da convolução na frequência #######################
+pylab.figure(figsize=(30, 25))
+pylab.gray()
+
+pylab.subplot(2, 2, 1), pylab.imshow(img), pylab.title(
+    'Imagem original', size=10), pylab.axis('off')
+
+pylab.subplot(2, 2, 4), pylab.imshow(blured_img)
+pylab.title('Imagem após convolução', size=10), pylab.axis('off')
+
+pylab.subplot(2, 2, 2), pylab.imshow(
+    (20*np.log10(0.1 + fp.fftshift(freq_kernel))).astype(int))
+pylab.title('Transformada do filtro', size=10), pylab.axis('off')
+
+pylab.subplot(2, 2, 3), pylab.imshow(
+    (20*np.log10(0.1 + fp.fftshift(convolved))).astype(int))
+pylab.title('Imagem borrada no domínio da frequência',
+            size=10), pylab.axis('off')
+
+pylab.subplots_adjust(wspace=0.2, hspace=1)
+pylab.show()
+
+##################################################################################################
+
+# lê o valor de k
+k = int(input("Digite o valor de k: "))
+
+# obtem a máscara (arestas)
+mask = cv2.subtract(img, blured_img)
+mask = k * mask
+
+# obtem imagem final
+final_image = cv2.add(img, mask)
+cv2.imwrite(input("Digite o caminho para a imagem de saída: "), final_image)
+print("Imagem final salva!")
--- s_highboost.py
+++ s_highboost.py
@@ -1,11 +1,42 @@
 import cv2
+from skimage import io
+import matplotlib.pylab as pylab
 
-# carrega a imagem image
-image = cv2.imread(input("Digite o caminho para a imagem de entrada: "))
+# carrega a imagem
+image = cv2.imread(input("Digite o caminho para a imagem de entrada: "))[
+    :, :, 0]
+
+# lê a dimensão do kernel
+n = int(input("Digite a dimensão da máscara: "))
+
+# lê o valor de k
+k = int(input("Digite o valor de k: "))
+
 # realiza borramento
-gauss = cv2.GaussianBlur(image, (7, 7), 0)
-# obtem a máscara
+gauss = cv2.GaussianBlur(image, (n, n), 0)
+
+# obtem a máscara (arestas)
 mask = cv2.subtract(image, gauss)
+mask = k * mask
+
+####################### Mostra todos os passos da convolução #######################
+pylab.figure(figsize=(30, 25))
+pylab.gray()
+
+pylab.subplot(2, 3, 1), pylab.imshow(image), pylab.title(
+    'Imagem original', size=10), pylab.axis('off')
+
+pylab.subplot(2, 3, 3), pylab.imshow(gauss)
+pylab.title('Imagem após convolução', size=10), pylab.axis('off')
+
+pylab.subplot(2, 3, 5), pylab.imshow(mask)
+pylab.title('Arestas para aguçamento', size=10), pylab.axis('off')
+
+pylab.subplots_adjust(wspace=0.2, hspace=1)
+pylab.show()
+##################################################################################################
+
 # obtem imagem final
 final_image = cv2.add(image, mask)
 cv2.imwrite(input("Digite o caminho para a imagem de saída: "), final_image)
+print("Imagem final salva!")
